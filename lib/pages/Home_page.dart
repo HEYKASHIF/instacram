201025706f425a12fc9921f76d1b4bdbea184d43
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-30 01:48:14 +0530

    30-march commit8

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -8,29 +8,34 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: appBarColor,
-        title: Text(
-          "instagram",
-          style: TextStyle(
-            fontFamily: 'Billabong',
-            fontSize: 35,
-          ),
-        ),
-        leading: Icon(
-          Icons.camera_alt_outlined,
-          color: Colors.white,
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(
-              Icons.send,
-            ),
-          )
-        ],
-      ),
+      appBar: getAppBar(),
     );
   }
+}
+
+AppBar getAppBar() {
+  return AppBar(
+    elevation: 0,
+    centerTitle: true,
+    backgroundColor: appBarColor,
+    title: Text(
+      "instagram",
+      style: TextStyle(
+        fontFamily: 'Billabong',
+        fontSize: 35,
+      ),
+    ),
+    leading: Icon(
+      Icons.camera_alt_outlined,
+      color: Colors.white,
+    ),
+    actions: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(
+          Icons.send,
+        ),
+      )
+    ],
+  );
 }
