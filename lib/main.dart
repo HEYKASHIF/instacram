6cc9e30df4e289a3e5ab6ecea31c3eff97a9a648
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-30 01:20:50 +0530

    30-march commit3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/Home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Homepage(),
+      home: HomePage(),
     );
   }
 }
